cdef4b4a2a0376717f6d87f6561b0e8ca6615806
Szalkowski <mac.szalkowski@gmail.com>
2016-04-14 08:30:51 +0200

    refs #6

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-var myVar;
+var delay;
 
 function myFunction() {
-    myVar = setTimeout(showPage, 2000);
+    delay = setTimeout(showPage, 2000);
 }
 
 function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("content").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
 }
  $(function(){
-        $('.hamburger').on('click', function(e){
-            e.stopPropagation();
-            $('.menu').toggle('slow');
-        });
-        $('body').on('click', function(){
-            $('.menu').hide();
-        });
-        $('.menu').on('click', function(e){
-            e.stopPropagation();
-        })
+    $('.hamburger').on('click', function(e){
+        e.stopPropagation();
+        $('.menu').toggle('slow');
+    });
+    $('body').on('click', function(){
+        $('.menu').hide();
+    });
+    $('.menu').on('click', function(e){
+        e.stopPropagation();
+    });
 });
 
